e61114e93f7a91370d5bb8098c7d2c64ff6d44a1
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2023-06-10 13:41:49 -0600

    Add function to calculate all days report

--- config.ts
+++ config.ts
@@ -1,17 +1,22 @@
 import { changeDateFormat } from "./libs/day-reporter.util";
 
 /**
- * List of dates to evaluate time
+ * @description List of dates to evaluate time
+ * example:
+ * [
+ * "2023-05-08",
+ * "2023-05-09",
+ * "2023-05-10",
+ * "2023-05-11",
+ * "2023-05-12",
+ * "2023-05-13",
+ * "2023-05-14",
+ * ]
+ * 
  */
-export const dates: string[] = changeDateFormat([
-  "2023-05-08",
-  "2023-05-09",
-  "2023-05-10",
-  "2023-05-11",
-  "2023-05-12",
-  "2023-05-13",
-  "2023-05-14",
-]);
+export const dates: string[] = changeDateFormat(
+   [ ]
+);
 
 /**
  * Note: Use Network tabs in devtools to filter by details?personId
@@ -22,5 +27,4 @@ export const personId: string = "1506950";
  * To update cookie use oficial openEnglish website to get a valid cookie
  * Note: Use Network tabs in devtools to filter by details?personId
  */
-export const cookie =
-  "liveagent_oref=https://student.openenglish.com/login.html; liveagent_ptid=a2bbcd1c-8959-4f6b-882c-17fd6f5145b0; _hjSessionUser_206797=eyJpZCI6IjFlOTdjYWU5LTIxYTItNWMzNy1iYTk1LWFjNTE0MzlkY2YxOCIsImNyZWF0ZWQiOjE2Njc4Mjk4MTQyNDgsImV4aXN0aW5nIjp0cnVlfQ==; _fbp=fb.1.1668831833802.654441413; _scid=b8011bb1-5bd8-4932-9160-b4671cfb5ffa; _gaexp=GAX1.2.rUsmjce7RSewpU2Bx_IjEg.19476.1!PylcKV1JRtq-ev93BQAPyg.19503.0; _scid_r=b8011bb1-5bd8-4932-9160-b4671cfb5ffa; _sctr=1%7C1681884000000; _clck=1igqyb0|1|faw|0; _ga_1PH2CJJXV0=GS1.1.1681923865.1.0.1681923865.0.0.0; _gcl_au=1.1.414885988.1683675539; _gid=GA1.2.276027161.1683907805; JSESSIONID=CB1D795E37633A02E2A58AF52263CE7C-n3; OESSOTOK=eyJhbGciOiJIUzUxMiJ9.eyJQRVJTT05fSUQiOjE1MDY5NTAsIkNPTlRBQ1RfSUQiOiIwMDMzcjAwMDAzd1pGdjNBQUciLCJST0xFX0lEUyI6WzFdLCJPUkdBTklaQVRJT04iOiJPcGVuIEVuZ2xpc2giLCJTQ09QRSI6WyJscDItdWkiLCJscDItd29sdmVyaW5lIiwicHAtdWkiXSwic3ViIjoib2Vfc3NvX3Rva2VuIiwiaXNzIjoiT3BlbiBFZHVjYXRpb24iLCJqdGkiOiI3ZTFhNjliMC0yNWRjLTRkMTUtYTRiZC1iOWVlMzc2MDY4ZDAiLCJpYXQiOjE2ODM5MDc4MTAsImV4cCI6MTY4NjQ5OTgxMH0.RUMFDh-w0m6zXYkSeyF4N7yzAhouqEFCEZ4r-wfhwvDVZRFGNrGhYodprnQTsx1F2sXn0eyPWxjEdIWx-Xbrfg; NLS=es; _opt_utmc=MX-GSN-Brand-Exact-adult; _gcl_aw=GCL.1683912822.CjwKCAjwx_eiBhBGEiwA15gLN-_Vb5lZ-r7xwSe4kMNVHqWMBr-ANT5_xT4BoAkGFuC7zWUetUFDBxoCJjAQAvD_BwE; __utmz=utmcsr=google|utmcmd=cpc_brand|utmccn=MX-GSN-Brand-Exact-adult|utmcct=RSA-Generic|utmctr=openenglish|utmgclid=CjwKCAjwx_eiBhBGEiwA15gLN-_Vb5lZ-r7xwSe4kMNVHqWMBr-ANT5_xT4BoAkGFuC7zWUetUFDBxoCJjAQAvD_BwE; __utmzsess=1; _gac_UA-2777546-17=1.1683912823.CjwKCAjwx_eiBhBGEiwA15gLN-_Vb5lZ-r7xwSe4kMNVHqWMBr-ANT5_xT4BoAkGFuC7zWUetUFDBxoCJjAQAvD_BwE; _gat=1; _hjIncludedInSessionSample_206797=0; _hjSession_206797=eyJpZCI6IjZkN2JmYTBlLTE5YTEtNDY4OC1hZDMyLWEyZGFjODgyMmFkMSIsImNyZWF0ZWQiOjE2ODM5MTI4MjI5MjEsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=0; _ga_QVVW4W1RHR=GS1.1.1683912822.77.0.1683912824.58.0.0; _gat_UA-2777546-20=1; _ga_HH1HJBMHQ7=GS1.1.1683912827.134.1.1683912831.0.0.0; _ga=GA1.2.533262748.1667604127; liveagent_sid=defe5b16-b7bb-49a0-9fd7-46fe966fb203; liveagent_vc=680";
+export const cookie = "" 
--- index.ts
+++ index.ts
@@ -2,14 +2,41 @@ import { cookie, dates, personId } from "./config";
 import { OpenEnglish } from "./libs/open-english";
 import { DayReporter } from "./libs/day-reporter";
 
-const rangeReporter = async () => {
+export const getActivityTypes = (days: string[]): string[] => {
+  let types: string[] = [];
+  days.forEach((day: any) => {
+    types = types.concat(day.types);
+  });
+
+  return [...new Set(types)];
+};
+
+(async () => {
   const days: any[] = [];
   for (const date of dates) {
     const { data } = await OpenEnglish(date, personId, cookie);
     const report = new DayReporter(data, date);
     days.push(report.getResult());
   }
-  console.log(days);
-};
 
-rangeReporter();
+  const types = getActivityTypes(days);
+  // Include mins to sum all time:
+  types.push("mins");
+
+  const totalReport: {[key: string]: any} = {};
+  days.forEach((day) => {
+    Object.keys(day).forEach((key: string) => {
+      if (types.includes(key)) {
+        totalReport[key] = !!totalReport[key]
+          ? totalReport[key] + day[key]
+          : day[key];
+      }
+    });
+  });
+
+  // Calculate total hours using minuts and saving in a new prop named hours
+  totalReport['hours'] =  totalReport['mins'] / 60;
+
+  // Show total report
+  console.log(totalReport);
+})();
